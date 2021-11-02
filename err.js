4b998f3e7a7a30c496ca2ce7f33058fce85f69f1
Timothy Wright-Bodine <timwbodine@gmail.com>
2021-11-02 15:50:22 -0400

    Update err.js

--- err.js
+++ err.js
@@ -1,7 +1,13 @@
 videojs.registerPlugin('errorText', function() {
-  var myPlayer = this;
+    var myPlayer = this;
 
-  myPlayer.errors({
-    "errors": false
-  });
+    myPlayer.errors({
+      "errors": {
+        "4": {
+          "headline": "The Live Stream will begin soon",
+          "type": "CUSTOM_TYPE",
+          "message": "Please come back, once the live event has begun!"
+        }
+      }
+    });
 });
