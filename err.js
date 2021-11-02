1a1603933b0581ee654d3da479559691cb220dc7
Timothy Wright-Bodine <timwbodine@gmail.com>
2021-11-02 14:09:04 -0400

    Create err.js

---
+++ err.js
@@ -0,0 +1,9 @@
+myPlayer.errors({
+  "errors": {
+    "4": {
+      "headline": "This is a custom error message",
+      "type": "custom type",
+      "message": "these are details"
+    }
+  }
+});
