948e09587dfd0e35292667b4610397289024161f
Timothy Wright-Bodine <timwbodine@gmail.com>
2021-11-02 14:13:07 -0400

    Update err.js

--- err.js
+++ err.js
@@ -1,9 +1,13 @@
-myPlayer.errors({
-  "errors": {
-    "4": {
-      "headline": "This is a custom error message",
-      "type": "custom type",
-      "message": "these are details"
+videojs.registerPlugin('errorText', function() {
+  var myPlayer = this;
+
+  myPlayer.errors({
+    "errors": {
+      "4": {
+        "headline": "The Live Stream will begin soon",
+        "type": "CUSTOM_TYPE",
+        "message": "Please come back, once the live event has begun!"
+      }
     }
-  }
+  });
 });
